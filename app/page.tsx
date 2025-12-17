afd8f0f5a12cd217ac1d3c9ac988aa83ca73835b
Potowai <81572547+Potowai@users.noreply.github.com>
2025-12-17 14:31:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        <h1 className="text-center text-4xl font-bold sm:text-left">
+          Welcome to Next.js!
+        </h1>
+        <p>
+          Get started by editing <code>app/page.tsx</code>
+        </p>
+      </main>
+    </div>
+  );
+}
