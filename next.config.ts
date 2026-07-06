8b2cb236584d2bfc1513516b279461458fefefd1
Potowai <81572547+Potowai@users.noreply.github.com>
2026-07-06 17:11:38 +0200

    fix: revert runtimeCaching to valid next-pwa config

--- next.config.ts
+++ next.config.ts
@@ -3,54 +3,6 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*\/hunt\/.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "hunt-pages",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 86400,
-        },
-        networkTimeoutSeconds: 5,
-      },
-    },
-    {
-      urlPattern: /^https:\/\/.*\/_next\/static\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "next-static",
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 604800,
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/.*\/images\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "images",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 604800,
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "others",
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 86400,
-        },
-        networkTimeoutSeconds: 5,
-      },
-    },
-  ],
 });
 
 const nextConfig: NextConfig = {};
